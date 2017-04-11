8de6ae7a2d6b54389499c333eab1f0077eb7e10a
Daniel Høyer Iversen <mail@dahoiv.net>
2017-04-11 18:15:42 +0200

    Update xiaomi.py

--- components/xiaomi.py
+++ components/xiaomi.py
@@ -425,6 +425,8 @@ class XiaomiDevice(Entity):
         max_volt = 3300
         min_volt = 2800
         voltage = data['voltage']
+        voltage = min(voltage, max_voltage)
+        voltage = max(voltage, min_voltage)
         percent = ((max_volt - voltage) / (max_volt - min_volt)) * 100
         self._device_state_attributes[ATTR_BATTERY_LEVEL] = round(percent)
         return True
